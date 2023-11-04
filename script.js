c8a9720eab339acd9c90873b369361f156bb7bda
jaideepsingh0085 <jaideepsingh.0085@gmail.com>
2023-11-04 23:41:16 +0530

    DOM

---
+++ script.js
@@ -0,0 +1,12 @@
+let container=document.getElementById("container");
+let box=document.getElementById("hello");
+container.style.backgroundColor="yellow";
+container.style.margin="20px";
+container.style.padding="10px";
+container.style.fontSize="18px";
+container.style.fontWeight="bold";
+container.style.height="200px";
+container.style.width="300px";
+box.style.backgroundColor="blue";
+box.style.width="128px";
+box.style.color="white";
